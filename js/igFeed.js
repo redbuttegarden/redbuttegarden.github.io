d7c4155ca3d9891cebe664d6552fef57625ccca2
DiegoAndino <diego.andino@redbutte.utah.edu>
2019-08-13 14:25:03 -0600

    iFrame test fix for Youtube (social page)

--- js/igFeed.js
+++ js/igFeed.js
@@ -6,7 +6,7 @@ var userFeed = new Instafeed({
   resolution: 'standard_resolution',
   accessToken: '227942832.403f1ba.7b1911b4b8634fcd90f27e7ce3600d47',
   sortBy: 'most-recent',
-  template: '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="{{link}}" target="_blank"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"/></div></a></div>'
+  template: '<a href="{{link}}" target="_blank"><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"/></div></div></a>'
 });
 
 userFeed.run();
