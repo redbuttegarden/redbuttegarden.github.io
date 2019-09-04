d28bf7ed25b58095587d4282557491ff58af8587
DiegoAndino <andino.diego98@gmail.com>
2019-09-04 15:42:15 -0600

    New and fixed Instagram feed

--- js/igFeed.js
+++ js/igFeed.js
@@ -1,12 +1,12 @@
 var userFeed = new Instafeed({
   get: 'user',
   userId: '227942832',
-  limit: 9,
+  limit: 12,
   link: 'https://www.instagram.com/redbuttegarden/',
   resolution: 'standard_resolution',
   accessToken: '227942832.403f1ba.7b1911b4b8634fcd90f27e7ce3600d47',
   sortBy: 'most-recent',
-  template: '<a href="{{link}}" target="_blank"><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"/></div></div></a>'
+  template: '<div class="col-lg-3 gallery instaimg"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
 });
 
 userFeed.run();
