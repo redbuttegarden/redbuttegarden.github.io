2871c8f334bbbbf8c6e6c3e0d42d8609c7412808
DiegoAndino <andino.diego98@gmail.com>
2019-09-06 11:29:11 -0600

    Social Media Update

---
+++ js/watchForHover.js
@@ -0,0 +1,34 @@
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.getElementsByClassName("img-fluid");
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
